b24cdf20f6b86bcc2da180e481ed99f90a94f23a
Clément SIPIETER <c.sipieter@gmail.com>
2011-10-27 16:51:47 +0200

    Add new applet: sound

---
+++ SoundModel.py
@@ -0,0 +1,21 @@
+
+from AppletModel import AppletModel
+
+try:
+  import alsaaudio as alsa
+except:
+  print "Module alsaaudio not found"
+
+class SoundModel(AppletModel):
+
+  def __init__(self, refresh_period):
+    AppletModel.__init__(self, refresh_period)
+    self.__vol = 0
+
+
+  def getVol(self):
+    return self.__vol
+
+
+  def update(self):
+    self.__vol = alsa.Mixer('Master').getvolume()[0]
---
+++ sound.py
@@ -0,0 +1,40 @@
+
+from AppletView import AppletView
+from SoundModel import SoundModel
+
+
+class sound(AppletView):
+
+
+  def __init__(self):
+    AppletView.__init__(self)
+    self.width = 40
+    self.height = 10
+    self.border_size = 1
+    self.bar_color = "#3F8EB4"
+    self.bg_color = "#555555"
+    self.border_color = "#000000"
+    self.img_path = ""
+    self.has_border = True
+
+
+  def init(self):
+    self.refresh_period = float(self.refresh_period)
+    self.width = int(self.width)
+    self.height = int(self.height)
+    self.border_size = int(self.border_size)
+    self.bar_color = str(self.bar_color)
+    self.bg_color = str(self.bg_color)
+    self.border_color = str(self.border_color)
+    self.img_path = str(self.img_path)
+    self.has_border = bool(self.has_border)
+    self.setModel( SoundModel( self.refresh_period ) )
+
+
+  def paint(self, g):
+    g.x -= self.width
+    y = (g.height - self.height) / 2
+    g.drawHBar(g.x, y, self.width, self.height, self.model.getVol()/100.0, self.bg_color, self.bar_color, self.border_color, self.border_size);
+    if(len(self.img_path) > 0):
+      g.x -= 12
+      g.drawImg(g.x, 1, 0, 0, self.img_path); 
