f3b23c0b9f8078383942cd95a9e211a5e70b0743
Clément SIPIETER <c.sipieter@gmail.com>
2011-11-13 22:37:20 +0100

    Add img_width option on sound applet

--- applets/sound.py
+++ applets/sound.py
@@ -15,7 +15,7 @@ class sound(AppletView):
     self.bg_color = "#555555"
     self.border_color = "#000000"
     self.img_path = ""
-    self.has_border = True
+    self.img_width = 0
 
 
   def init(self):
@@ -27,7 +27,7 @@ class sound(AppletView):
     self.bg_color = str(self.bg_color)
     self.border_color = str(self.border_color)
     self.img_path = str(self.img_path)
-    self.has_border = bool(self.has_border)
+    self.img_width = int(self.img_width)
     self.setModel( SoundModel( self.refresh_period ) )
 
 
@@ -35,6 +35,7 @@ class sound(AppletView):
     g.x -= self.width
     y = (g.height - self.height) / 2
     g.drawHBar(g.x, y, self.width, self.height, self.model.getVol()/100.0, self.bg_color, self.bar_color, self.border_color, self.border_size);
+    
     if(len(self.img_path) > 0):
-      g.x -= 12
+      g.x -= self.img_width
       g.drawImg(g.x, 1, 0, 0, self.img_path); 
